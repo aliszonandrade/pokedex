a158d3c0a520af3387028b9c10a8ec7fc420cac2
Alisson Andrade <alisson14101998@gmail.com>
2022-01-07 16:30:46 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title is-1 has-text-white-ter">Pokédex</h1>
+    <input type="text" class="column is-half is-offset-one-quarter input is-rounded" name="" id="" placeholder="Buscar pokémon pelo nome" v-model="find">
+    <button id="btnFind" class="column is-half is-offset-one-quarter button is-info is-rounded" @click="findPokemon">Buscar</button>
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="(poke,index) in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1" />
+    </div>
+    </div>    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import Pokemon from './components/Pokemon.vue';
 
 export default {
   name: 'App',
+  data(){
+    return {
+      pokemons: [],
+      filteredPokemons: [],
+      find: ''
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
+    }).catch();
+  },
   components: {
-    HelloWorld
+    Pokemon
+  },
+  methods: {
+    findPokemon: function(){
+      this.filteredPokemons = this.pokemons;
+
+      if(this.find == '' || this.find == " "){
+          this.filteredPokemons = this.pokemons;
+        }else{
+          this.filteredPokemons =  this.pokemons.filter(poke => poke.name == this.find.toLowerCase());
+        }
+    }
   }
 }
 </script>
@@ -23,6 +54,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: lightskyblue;
+}
+
+#btnFind{
+
+  margin-top: 1%;
 }
 </style>
